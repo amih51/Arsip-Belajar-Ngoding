8bbfb65bcae747d048d99e5bac99a3d38fb80fd2
amih51 <takpenting.noname@gmail.com>
2023-07-21 11:00:59 +0700

    -

---
+++ Codeforces/800to1300/lightsOut.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int template[3][3] = {(1,1,1),(1,1,1),(1,1,1)};
+
+    int input[3][3];
+    for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                cin >> input[i][j];
+            }
+        }
+}
